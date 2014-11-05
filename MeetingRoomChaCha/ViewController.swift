f8efe56906331f726f05106bed13d0b1af76d8d1
Alex Ong <aong@thoughtworks.com>
2014-11-05 20:43:05 +1100

    Initial commit

---
+++ MeetingRoomChaCha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeetingRoomChaCha
+//
+//  Created by Alex Ong on 5/11/2014.
+//  Copyright (c) 2014 Alex Ong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
